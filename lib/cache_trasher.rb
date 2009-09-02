c912ec71236ad803fb4666d43757741f87db1428
Adrian Duyzer <adrianduyzer@gmail.com>
2009-09-02 14:36:13 -0400

    added formats as an option

--- lib/cache_trasher.rb
+++ lib/cache_trasher.rb
@@ -2,16 +2,22 @@
 module ActionController::Caching::Pages
   module ClassMethods
   
-    def trash_cache(path)
+    def trash_cache(path, options)
       return unless perform_caching
       
       # check if the action or controller has been saved as a cached file
       # as opposed to just a directory.
       paths = [path.chomp('/')]
-      paths << "#{paths[0]}#{page_cache_extension}" if File.exist?("#{page_cache_directory}/#{paths[0]}#{page_cache_extension}")
-
+      # if options includes format(s), then use those, otherwise use the standard page_cache_extension to pick stuff up
+      if options[:format]
+        options[:format].to_a.each do |format|
+          paths << "#{paths[0]}.#{format}" if File.exist?("#{page_cache_directory}/#{paths[0]}.#{format}")
+        end
+      else
+        paths << "#{paths[0]}#{page_cache_extension}" if File.exist?("#{page_cache_directory}/#{paths[0]}#{page_cache_extension}")
+      end
       paths.each do |p|
-        p = page_cache_directory + p
+        p = File.join(page_cache_directory, p)
         unless [p, p.chomp, p.chomp('/'), p.chomp('/*')].include? Rails.public_path 
           benchmark "Removing cache file or directory: #{p}" do
             FileUtils.rm_r(p) rescue Errno::ENOENT
@@ -25,16 +31,16 @@ module ActionController::Caching::Pages
     
   end
   
-  def trash_cache(options = "")
+  def trash_cache(supplied_path = "", options = {})
     return unless perform_caching
     
-    path = case options
+    path = case supplied_path
     when Hash
       url_for(options.merge(:only_path => true, :skip_relative_url_root => true))
     when String
-      options
+      supplied_path
     end
     
-    self.class.trash_cache(path)
+    self.class.trash_cache(path, options)
   end
 end
